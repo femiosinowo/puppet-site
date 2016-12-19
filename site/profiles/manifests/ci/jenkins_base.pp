efe0e05d8057d8cd192cb9234f572295edef5df7
femi osinowo <fosinowo@governmentcio.com>
2016-12-19 00:20:27 -0500

    adding password to jenkins

--- site/profiles/manifests/ci/jenkins_base.pp
+++ site/profiles/manifests/ci/jenkins_base.pp
@@ -1,6 +1,6 @@
 class profiles::jenkins_base (
-  $jenkins_username = hiera('jenkins::jenkins_username'),
-  $jenkins_password = hiera('jenkins::jenkins_password')) {
+  $jenkins_username = hiera('jenkins::jenkins_ui_username'),
+  $jenkins_password = hiera('jenkins::jenkins_ui_password')) {
   class { 'jenkins': }
 
 }
--- site/profiles/manifests/ci/jenkins_master.pp
+++ site/profiles/manifests/ci/jenkins_master.pp
@@ -1,4 +1,9 @@
-class profiles::ci::jenkins_master () {
+class profiles::ci::jenkins_master (
+  
+   $jenkins_username = hiera('jenkins::jenkins_username'),
+  $jenkins_password = hiera('jenkins::jenkins_password'),
+  
+) {
   include jenkins
   include jenkins::master
 
